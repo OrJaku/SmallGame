64a5efd32cbee4ebcd7723313122c1546b4db455
OrJaku <51891265+OrJaku@users.noreply.github.com>
2020-09-14 21:23:57 +0200

    Add new file

---
+++ main.cs
@@ -0,0 +1,60 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Welcome!");
+    Console.WriteLine ("Write your name:");
+    string username = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(username)){
+        username = "Stranger";
+        Console.WriteLine ("So you are" + username);
+    }
+    Console.Clear();
+    Console.WriteLine ("Hello " + username );
+    Console.WriteLine ("  ");
+    string[] level = {
+      "##########",
+      "#  #######",
+      "#   ######",
+      "##   #####",
+      "##  ######",
+      "##     ###",
+      "##     ###",
+      "## #######",
+      "## #######",
+      "##       #"
+    };
+    foreach(string row in level){
+      Console.WriteLine(row);
+    }
+    int positionRow = 3;
+    int positionCol = 2;
+    while(true){
+      Console.SetCursorPosition(positionCol, positionRow);
+      Console.Write("@");
+      Console.SetCursorPosition(0, level.Length + 2);
+      ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+      Console.SetCursorPosition(positionCol, positionRow);
+      Console.Write(" ");      
+      if(keyInfo.Key == ConsoleKey.LeftArrow)
+      {
+        positionCol--;   
+      }
+      else if(keyInfo.Key == ConsoleKey.RightArrow)
+      {
+        positionCol++;  
+      }
+      else if(keyInfo.Key == ConsoleKey.UpArrow)
+      {
+        positionRow--;     
+      }
+      else if(keyInfo.Key == ConsoleKey.DownArrow)
+      {
+        positionRow++;     
+      }
+    }
+    Console.WriteLine ("  ");
+    Console.WriteLine ("Press any key to quit");
+    Console.ReadKey(true);
+  }
+}
